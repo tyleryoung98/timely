ea38280fbb3ff90650c49e31e91e2e6f1903301e
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-02 01:05:57 -0400

    Update app.js
    11/1 presentation version

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/routes')(db)
+const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//link to .env
+if(process.env.NODE_ENV != 'production'){
+  require('dotenv').config();
+  //console.log('did it work');
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.use('/', routes);
+app.use('/', routes());
 
 
-
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true }).then(() => {
+  console.log('connected to mongodb');
+});
 //put this last
 app.listen(3000, () => console.log('App listening'));
