ee698ba506c4a24005ce8b3436c36dea7ed19c83
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-27 17:11:00 -0500

    updated

--- mongo.js
+++ mongo.js
@@ -6,8 +6,14 @@ module.exports = {
   signup: function(email, first_name, last_name, password){
     return new Promise((resolve, reject) =>{
     User.create({email, first_name, last_name, password}).then(() => {
-      resolve();
+      resolve();});
     });
-  });
-}
+  }
+  login: function(email, password){
+    return new Promise((resolve, reject)=>{
+      User.find({'email' : email}).then(()=>{
+        if()
+      });
+    })
+  }
 }
