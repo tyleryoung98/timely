ad9fe98fe68ab29e475454e093c29ecf1d819bf5
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-26 22:47:31 -0500

    Added login
    NOT FINISHED

--- mongo.js
+++ mongo.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 
 const User = require('./userInfo/user.js');
+//const Mailer = require('./mailing.js');
 
 module.exports = {
   signup: function(email, first_name, last_name, password){
     return new Promise((resolve, reject) =>{
-    User.create({email, first_name, last_name, password}).then(() => {
-      resolve();
+      User.create({email, first_name, last_name, password, school, major}).then(() =>{
+        resolve();
+      });
     });
-  });
-}
+  },
+  login: function(mail, pass){
+    return new Promise((resolve, reject)=>{
+      User.findOne({email : mail}, function(err, user){
+        if(err) throw err;
+        user.comparePassword(pass, function(err, isMatch){
+          if(err) throw err;
+          console.log('Password:', isMatch);
+        });
+      }).then(()=>{
+        resolve();
+      });
+    })
+  }
 }
