1da3d4eaed05ca87f9e60de530040b36c7087f1b
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-26 22:50:14 -0500

    Updated for login
    Not working properly

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,13 @@ module.exports = function(){
     db.signup(email, first_name, last_name, password).then(()=>{
       res.send('Account Created')
     });
+  }),
+  router.post('/assignments.html', function(req, res){
+    console.log(req.body);
+    const { email, password } = req.body;
+    db.login(email, password).then(()=>{
+      res.send('Logged in successfully')
+    });
   })
   return router;
 }
