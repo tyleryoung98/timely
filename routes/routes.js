8ba4938a47d7a389cf0388f9b0daf42f8588ebc8
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-27 17:11:37 -0500

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,13 @@ module.exports = function(){
       res.send('Account Created')
     });
   })
+
+  router.post('/login', function(req, res){
+    console.log(req.body);
+    const{ email, password} = req.body;
+    db.login(email, password).then(()=>{
+      res.send('Login Successful')
+    });
+  })
   return router;
 }
