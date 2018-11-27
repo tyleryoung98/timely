4764bc5cb80220b531bc0d4febb7078c08444229
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-27 17:13:15 -0500

    Add files via upload

--- mongo.js
+++ mongo.js
@@ -1,26 +1,18 @@
 const mongoose = require('mongoose');
 
 const User = require('./userInfo/user.js');
-//const Mailer = require('./mailing.js');
 
 module.exports = {
   signup: function(email, first_name, last_name, password){
     return new Promise((resolve, reject) =>{
-      User.create({email, first_name, last_name, password, school, major}).then(() =>{
-        resolve();
-      });
+    User.create({email, first_name, last_name, password}).then(() => {
+      resolve();});
     });
-  },
-  login: function(mail, pass){
+  }
+  login: function(email, password){
     return new Promise((resolve, reject)=>{
-      User.findOne({email : mail}, function(err, user){
-        if(err) throw err;
-        user.comparePassword(pass, function(err, isMatch){
-          if(err) throw err;
-          console.log('Password:', isMatch);
-        });
-      }).then(()=>{
-        resolve();
+      User.find({'email' : email}).then(()=>{
+        if()
       });
     })
   }
