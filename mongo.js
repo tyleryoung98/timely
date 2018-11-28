08bcaf2cb814cb90033e2854c5009baf5b59d7d6
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-27 22:29:36 -0500

    Updated mongo

--- mongo.js
+++ mongo.js
@@ -1,27 +1,57 @@
 const mongoose = require('mongoose');
-
+let bcrypt = require('bcrypt');
 const User = require('./userInfo/user.js');
+let jwt = require('jsonwebtoken');
 //const Mailer = require('./mailing.js');
 
-module.exports = {
-  signup: function(email, first_name, last_name, password){
+module.exports = class Mongo {
+  signup(email, first_name, last_name, password){
     return new Promise((resolve, reject) =>{
+      let hashedPassword = bcrypt.hashSync(pass, 8);
       User.create({email, first_name, last_name, password}).then(() =>{
         resolve();
       });
     });
-  },
-  login: function(mail, pass){
+  }
+
+  login(mail, pass){
+    const self = this;
     return new Promise((resolve, reject)=>{
       User.findOne({email : mail}, function(err, user){
         if(err) throw err;
+
         user.comparePassword(pass, function(err, isMatch){
           if(err) throw err;
           console.log('Password:', isMatch);
-          if(isMatch) resolve();
+          let token = self.token(user);
+          console.log(token);
+          if(isMatch) resolve(token);
           else reject();
         });
       });
     })
   }
+
+  token(user){
+    const payload = {
+      id: user._id,
+      email: user.email
+    };
+    return jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: '10y'
+    });
+  }
+
+  verifyToken(token){
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, SECRET_KEY, { version: TOKEN_VERSION }, function(err, decoded) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(decoded);
+        }
+      });
+    });
+  }
+
 }
