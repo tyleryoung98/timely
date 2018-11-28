6fccb8df0e5574ad1bab2896f3ef3987f8488b89
tyleryoung98 <44182533+tyleryoung98@users.noreply.github.com>
2018-11-27 22:28:47 -0500

    Updated routes with verification

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
-const db = require('../mongo.js');
+const Mongo = require('../mongo.js');
+let bcrypt = require('bcrypt');
+let db = new Mongo();
+const isAuthenticated = function (req, res, next) {
+    db.verifyToken(req.headers['x-access-token']).then(next)
+                                .catch(() => res.redirect('./index.html'));
+};
+const path = require('path');
 
 module.exports = function(){
   router.post('/signup', function(req, res){
@@ -9,14 +16,34 @@ module.exports = function(){
       res.send('Account Created')
     });
   }),
-  router.post('/assignments.html', function(req, res){
+
+  router.post('/login', function(req, res){
     console.log(req.body);
     const { email, password } = req.body;
     db.login(email, password).then(()=>{
-      res.redirect('./assignments.html')
+      res.redirect('/assignments')
     }).catch(()=>{
       res.redirect('./index.html')
     });
+  }),
+  router.get('/super-secret-info', isAuthenticated, function(req, res) {
+        //if isAuthenticated calls next(), the request proceeds here
+  });
+  router.get('/assignments', isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/html','assignments.html'));
+  });
+  router.get('/about', isAuthenticated, function(req, res) {
+    res.sendFile('./about.html');
+  });
+  router.get('/classes', isAuthenticated, function(req, res) {
+    res.sendFile('./classes.html');
+  });
+  router.get('/profile', isAuthenticated, function(req, res) {
+    res.sendFile('profile.html');
+  });
+  router.get('/', function(req, res){
+    //console.log(__dirname);
+    res.sendFile(path.join(__dirname, '../public/html','index.html'));
   })
   return router;
 }
